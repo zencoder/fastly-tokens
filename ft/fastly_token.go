e78cf4794cde088c04792789e98ed5bed7943884
Scott Kidder <skidder@brightcove.com>
2015-04-15 15:03:45 -0700

    Rename variables to use camelcase

--- ft/fastly_token.go
+++ ft/fastly_token.go
@@ -10,23 +10,23 @@ import (
 	"time"
 )
 
-func GenerateToken(secret string, seconds_valid int, encoding *base64.Encoding) (token string, err error) {
-	return GenerateTokenForURL("", secret, seconds_valid, encoding)
+func GenerateToken(secret string, secondsValid int, encoding *base64.Encoding) (token string, err error) {
+	return GenerateTokenForURL("", secret, secondsValid, encoding)
 }
 
-func GenerateTokenForURL(url string, secret string, seconds_valid int, encoding *base64.Encoding) (token string, err error) {
-	var secret_bytes []byte
-	if secret_bytes, err = base64.StdEncoding.DecodeString(secret); err != nil {
+func GenerateTokenForURL(url string, secret string, secondsValid int, encoding *base64.Encoding) (token string, err error) {
+	var secretBytes []byte
+	if secretBytes, err = base64.StdEncoding.DecodeString(secret); err != nil {
 		return "", err
 	}
 
-	var secret_buffer bytes.Buffer
-	secret_buffer.Write(secret_bytes)
-	secret_buffer.WriteString(url)
+	var secretBuf bytes.Buffer
+	secretBuf.Write(secretBytes)
+	secretBuf.WriteString(url)
 
 	buf := new(bytes.Buffer)
-	binary.Write(buf, binary.LittleEndian, uint64(time.Now().Unix()/int64(seconds_valid)))
-	mac := hmac.New(sha256.New, secret_buffer.Bytes())
+	binary.Write(buf, binary.LittleEndian, uint64(time.Now().Unix()/int64(secondsValid)))
+	mac := hmac.New(sha256.New, secretBuf.Bytes())
 	mac.Write(buf.Bytes())
 	token = strings.TrimSpace(encoding.EncodeToString(mac.Sum(nil)))
 
